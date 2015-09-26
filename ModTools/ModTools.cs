d768be52176c799bf90b04bfc2356b7664c70c78
Luuk Holleman <luukholleman@gmail.com>
2015-09-26 17:36:57 +0200

    Added debugging console

---
+++ ModTools/ModTools.cs
@@ -0,0 +1,25 @@
+﻿using ModTools.Tools;
+using UnityEngine;
+
+namespace ModTools
+{
+    public class ModTools : IMod
+    {
+        private GameObject _go;
+
+        public void onEnabled()
+        {
+            _go = new GameObject();
+
+            _go.AddComponent<Console>();
+        }
+
+        public void onDisabled()
+        {
+
+        }
+
+        public string Name { get { return "ModTools"; } }
+        public string Description { get { return "Helpers for mods"; } }
+    }
+}
